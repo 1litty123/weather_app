8145cd7849322675e538191e21722fc758ea57e0
Bea Litonjua <bealitonjua9@gmail.com>
2018-07-02 20:35:59 +0100

    final changes

--- app.js
+++ app.js
@@ -41,11 +41,13 @@ var numberOfWeek = d.getDay();
 var daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 var dayOfWeek = daysOfTheWeek[numberOfWeek];
 var year = d.getFullYear();
-var moreInfo = app.querySelector('.more-info')
+var moreInfo = app.querySelector('.more-info');
 cityInput.value = "Pick a City."
+var backInfo = app.querySelector('.back-info')
 
 
 cityInput.addEventListener('click', function() {
+    var city = cityInput.value;
     var defaultBounds = new google.maps.LatLngBounds(
     new google.maps.LatLng(-33.8902, 151.1759),
     new google.maps.LatLng(-33.8474, 151.2631));
@@ -55,79 +57,6 @@ cityInput.addEventListener('click', function() {
       };
     autocomplete = new google.maps.places.Autocomplete(cityInput, options); 
     google.maps.event.addListener(autocomplete, 'place_changed', function () {
-        cityWeather.innerHTML = "<img src=\loading.gif\></img>";
-        var city = cityInput.value;
-        getCoordinatesForCity(city)
-        .then(getCurrentWeather)
-        .then(function(weather) {
-            var condition;
-            var condition;
-            if (weather.currently.icon === "clear-day") {
-                condition = "<i class='wi wi-day-sunny' style='color:#FFFEA6'></i>";
-            } else if (weather.currently.icon === "clear-night") {
-                condition = "<i class='wi wi-night-clear' style='color:#0042AD;'></i>";
-            } else if (weather.currently.icon === "rain") {
-                condition = "<i class='wi wi-day-rain'></i>";
-            } else if (weather.currently.icon === "snow") {
-                condition = "<i class='wi wi-snow'></i>";
-            } else if (weather.currently.icon === "sleet") {
-                condition = "<i class='wi wi-sleet'></i>";
-            } else if (weather.currently.icon === "wind") {
-                condition =  "<i class='wi wi-strong-wind'></i>";
-            } else if (weather.currently.icon === "fog") {
-                condition = "<i class='wi wi-fog'></i>";
-            } else if (weather.currently.icon === "cloudy") {
-                condition = "<i class='wi wi-cloudy'></i>";
-            } else if (weather.currently.icon === "partly-cloudy-day") {
-                condition = "<i class='wi wi-day-cloudy'></i>";
-            } else { // partly cloudy night
-                condition = "<i class='wi wi-night-alt-partly-cloudy'></i>";
-            }
-            cityWeather.innerHTML = condition + ' ' + weather.currently.temperature.toPrecision(3) + '°C'; 
-            cityInfo.innerHTML = '<br><span>' + dayOfWeek + ', ' + month + ' ' + numberOfMonth + ' ' + year + '</span></b><br><b>Precipitation:</b> ' + weather.currently.precipIntensity.toFixed(2) * 100 + 'mm/h' + '<br><b>Humidity:</b> ' + weather.currently.humidity.toFixed(2) * 100 + '%' + '<br><b>Wind:</b> ' + weather.currently.windSpeed.toFixed(2) + 'm/s <br>';
-            moreInfo.innerHTML = '';
-            moreInfoClick;
-            console.log(weather.daily.data);
-            });
-        cityInput.blur();
-      });
-    if (cityInput.value == "Pick a City.") {
-        cityInput.value = ''
-        cityInput.style.textTransform = "capitalize";
-    } else {
-        cityInput.value = '';
-        cityWeather.innerHTML = '';
-        cityInfo.innerHTML = '';
-        moreInfo.innerHTML = '';
-    }
-})
-
-var moreInfoClick = moreInfo.addEventListener('click', function() {
-    var city = cityInput.value;
-    var avgTemperature;
-    var i;
-    var avgArray = [];
-    getCoordinatesForCity(city)
-    .then(getCurrentWeather)
-    .then(function(weather) {
-        for (i = 1; i < weather.daily.data.length; i++) {
-            avgArray = avgArray + [averages[i]];
-            var averages = (average(weather.daily.data[i].temperatureHigh,weather.daily.data[i].temperatureLow)).toPrecision(3);
-            console.log(averages);
-            moreInfo.innerHTML = averages;
-        }
-    }
-)
-})
-
-function average(x,y) {
-    return (x + y) / 2;
-}
-
-
-cityForm.addEventListener('submit', function(event) { // this line changes
-    event.preventDefault(); // prevent the form from submitting
-    // This code doesn't change!
     cityWeather.innerHTML = "<img src=\loading.gif\></img>";
     var city = cityInput.value;
     getCoordinatesForCity(city)
@@ -156,10 +85,184 @@ cityForm.addEventListener('submit', function(event) { // this line changes
             condition = "<i class='wi wi-night-alt-partly-cloudy'></i>";
         }
         cityWeather.innerHTML = condition + ' ' + weather.currently.temperature.toPrecision(3) + '°C'; 
+        cityInfo.innerHTML = '<br><span>' + dayOfWeek +'</span></b><br><b>Precipitation:</b> ' + weather.currently.precipIntensity.toFixed(2) * 100 + 'mm/h' + '<br><b>Humidity:</b> ' + weather.currently.humidity.toFixed(2) * 100 + '%' + '<br><b>Wind:</b> ' + weather.currently.windSpeed.toFixed(2) + 'm/s <br>';
+        moreInfo.innerHTML = ">";
+        moreInfoClick;
+        console.log(weather.daily.data);
+          });
+          cityInput.blur();
+        });
+    if (cityInput.value == "Pick a City.") {
+        cityInput.value = ''
+        cityInput.style.textTransform = "capitalize";
+    } else {
+        cityInput.value = '';
+        cityWeather.innerHTML = '';
+        cityInfo.innerHTML = '';
+        backInfo.innerHTML = '';
+        moreInfo.innerHTML = '';
+     
+    }
+})
+
+var addition = 0;
+var moreInfoClick = moreInfo.addEventListener('click', function() {
+    backInfo.innerHTML = "<";
+    cityWeather.innerHTML = "<img src=\loading.gif\></img>";
+    cityInfo.innerHTML = "";
+    addition++;
+    if (addition === 6) {
+        moreInfo.innerHTML = '';
+    }
+    var city = cityInput.value;
+    getCoordinatesForCity(city)
+    .then(getCurrentWeather)
+    .then(function(weather) {
+        var condition;
+        if (weather.daily.data[addition].icon === "clear-day") {
+            condition = "<i class='wi wi-day-sunny' style='color:#FFFEA6'></i>";
+        } else if (weather.daily.data[addition].icon === "clear-night") {
+            condition = "<i class='wi wi-night-clear' style='color:#0042AD;'></i>";
+        } else if (weather.daily.data[addition].icon === "rain") {
+            condition = "<i class='wi wi-day-rain'></i>";
+        } else if (weather.daily.data[addition].icon === "snow") {
+            condition = "<i class='wi wi-snow'></i>";
+        } else if (weather.daily.data[addition].icon === "sleet") {
+            condition = "<i class='wi wi-sleet'></i>";
+        } else if (weather.daily.data[addition].icon === "wind") {
+            condition =  "<i class='wi wi-strong-wind'></i>";
+        } else if (weather.daily.data[addition].icon === "fog") {
+            condition = "<i class='wi wi-fog'></i>";
+        } else if (weather.daily.data[addition].icon === "cloudy") {
+            condition = "<i class='wi wi-cloudy'></i>";
+        } else if (weather.daily.data[addition].icon === "partly-cloudy-day") {
+            condition = "<i class='wi wi-day-cloudy'></i>";
+        } else { // partly cloudy night
+            condition = "<i class='wi wi-night-alt-partly-cloudy'></i>";
+        } if (numberOfWeek + 1 === 7) {
+            numberOfWeek = 0
+        } else {
+            numberOfWeek++
+        }
+        cityWeather.innerHTML = condition + '<span class="lowHigh">' + weather.daily.data[addition].temperatureHigh.toPrecision(3) + '°C ' + weather.daily.data[addition].temperatureLow.toPrecision(3) + '°C </span>'; 
+        cityInfo.innerHTML = '<br><span>' + daysOfTheWeek[numberOfWeek] + '</span></b><br><b>Precipitation:</b> ' + weather.daily.data[addition].precipIntensity.toFixed(2) * 100 + 'mm/h' + '<br><b>Humidity:</b> ' + weather.daily.data[addition].humidity.toFixed(2) * 100 + '%' + '<br><b>Wind:</b> ' + weather.daily.data[addition].windSpeed.toFixed(2) + 'm/s<br>';
+    
+        console.log(addition);
+        })
+    });
+
+
+backInfo.addEventListener('click', function() {
+    moreInfo.innerHTML = ">";
+    cityWeather.innerHTML = "<img src=\loading.gif\></img>";
+    cityInfo.innerHTML = "";
+    addition--;
+    if (addition > 0) {
+        backInfo.innerHTML = "<";
+    } else {
+        backInfo.innerHTML = '';
+    }
+    var city = cityInput.value;
+    getCoordinatesForCity(city)
+    .then(getCurrentWeather)
+    .then(function(weather) {
+        var condition;
+        if (weather.daily.data[addition].icon === "clear-day") {
+            condition = "<i class='wi wi-day-sunny' style='color:#FFFEA6'></i>";
+        } else if (weather.daily.data[addition].icon === "clear-night") {
+            condition = "<i class='wi wi-night-clear' style='color:#0042AD;'></i>";
+        } else if (weather.daily.data[addition].icon === "rain") {
+            condition = "<i class='wi wi-day-rain'></i>";
+        } else if (weather.daily.data[addition].icon === "snow") {
+            condition = "<i class='wi wi-snow'></i>";
+        } else if (weather.daily.data[addition].icon === "sleet") {
+            condition = "<i class='wi wi-sleet'></i>";
+        } else if (weather.daily.data[addition].icon === "wind") {
+            condition =  "<i class='wi wi-strong-wind'></i>";
+        } else if (weather.daily.data[addition].icon === "fog") {
+            condition = "<i class='wi wi-fog'></i>";
+        } else if (weather.daily.data[addition].icon === "cloudy") {
+            condition = "<i class='wi wi-cloudy'></i>";
+        } else if (weather.daily.data[addition].icon === "partly-cloudy-day") {
+            condition = "<i class='wi wi-day-cloudy'></i>";
+        } else { // partly cloudy night
+            condition = "<i class='wi wi-night-alt-partly-cloudy'></i>";
+        } if (numberOfWeek - 1 === -1) {
+            numberOfWeek = 6
+        } else {
+            numberOfWeek--
+        } if (addition === 0) {
+            var condition;
+            if (weather.currently.icon === "clear-day") {
+                condition = "<i class='wi wi-day-sunny' style='color:#FFFEA6'></i>";
+            } else if (weather.currently.icon === "clear-night") {
+                condition = "<i class='wi wi-night-clear' style='color:#0042AD;'></i>";
+            } else if (weather.currently.icon === "rain") {
+                condition = "<i class='wi wi-day-rain'></i>";
+            } else if (weather.currently.icon === "snow") {
+                condition = "<i class='wi wi-snow'></i>";
+            } else if (weather.currently.icon === "sleet") {
+                condition = "<i class='wi wi-sleet'></i>";
+            } else if (weather.currently.icon === "wind") {
+                condition =  "<i class='wi wi-strong-wind'></i>";
+            } else if (weather.currently.icon === "fog") {
+                condition = "<i class='wi wi-fog'></i>";
+            } else if (weather.currently.icon === "cloudy") {
+                condition = "<i class='wi wi-cloudy'></i>";
+            } else if (weather.currently.icon === "partly-cloudy-day") {
+                condition = "<i class='wi wi-day-cloudy'></i>";
+            } else { // partly cloudy night
+                condition = "<i class='wi wi-night-alt-partly-cloudy'></i>";
+            }
+            cityWeather.innerHTML = condition + ' ' + weather.currently.temperature.toPrecision(3) + '°C'; 
+            cityInfo.innerHTML = '<br><span>' + dayOfWeek +'</span></b><br><b>Precipitation:</b> ' + weather.currently.precipIntensity.toFixed(2) * 100 + 'mm/h' + '<br><b>Humidity:</b> ' + weather.currently.humidity.toFixed(2) * 100 + '%' + '<br><b>Wind:</b> ' + weather.currently.windSpeed.toFixed(2) + 'm/s <br>';
+            
+        } else {
+        cityWeather.innerHTML = condition + '<span class="lowHigh">' + weather.daily.data[addition].temperatureHigh.toPrecision(3) + '°C ' + weather.daily.data[addition].temperatureLow.toPrecision(3) + '°C </span>'; 
+        cityInfo.innerHTML = '<br><span>' + daysOfTheWeek[numberOfWeek] + '</span></b><br><b>Precipitation:</b> ' + weather.daily.data[addition].precipIntensity.toFixed(2) * 100 + 'mm/h' + '<br><b>Humidity:</b> ' + weather.daily.data[addition].humidity.toFixed(2) * 100 + '%' + '<br><b>Wind:</b> ' + weather.daily.data[addition].windSpeed.toFixed(2) + 'm/s<br>';
+        }
+        console.log(addition);
+        })
+    });
+    
+
+
+cityForm.addEventListener('submit', function(event) { // this line changes
+    event.preventDefault();
+    cityWeather.innerHTML = "<img src=\loading.gif\></img>";
+
+    var city = cityInput.value;
+    getCoordinatesForCity(city)
+    .then(getCurrentWeather)
+    .then(function(weather) {
+        var condition;
+        if (weather.currently.icon === "clear-day") {
+            condition = "<i class='wi wi-day-sunny' style='color:#FFFEA6'></i>";
+        } else if (weather.currently.icon === "clear-night") {
+            condition = "<i class='wi wi-night-clear' style='color:#0042AD;'></i>";
+        } else if (weather.currently.icon === "rain") {
+            condition = "<i class='wi wi-day-rain'></i>";
+        } else if (weather.currently.icon === "snow") {
+            condition = "<i class='wi wi-snow'></i>";
+        } else if (weather.currently.icon === "sleet") {
+            condition = "<i class='wi wi-sleet'></i>";
+        } else if (weather.currently.icon === "wind") {
+            condition =  "<i class='wi wi-strong-wind'></i>";
+        } else if (weather.currently.icon === "fog") {
+            condition = "<i class='wi wi-fog'></i>";
+        } else if (weather.currently.icon === "cloudy") {
+            condition = "<i class='wi wi-cloudy'></i>";
+        } else if (weather.currently.icon === "partly-cloudy-day") {
+            condition = "<i class='wi wi-day-cloudy'></i>";
+        } else { // partly cloudy night
+            condition = "<i class='wi wi-night-alt-partly-cloudy'></i>";
+        }
+        cityWeather.innerHTML = condition + ' ' + weather.currently.temperature.toPrecision(3) + '°C'; 
         cityInfo.innerHTML = '<br><span>' + dayOfWeek + ', ' + month + ' ' + numberOfMonth + ' ' + year + '</span></b><br><b>Precipitation:</b> ' + weather.currently.precipIntensity.toFixed(2) * 100 + 'mm/h' + '<br><b>Humidity:</b> ' + weather.currently.humidity.toFixed(2) * 100 + '%' + '<br><b>Wind:</b> ' + weather.currently.windSpeed.toFixed(2) + 'm/s <br>';
+        moreInfo.innerHTML = '';
+        moreInfoClick;
         console.log(weather.daily.data);
         });
-    cityInput.blur(); 
+        cityInput.blur(); 
   });
-
 
